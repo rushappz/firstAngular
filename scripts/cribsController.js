81f5bf0bdf728d232d515095ea8774bf310fe310
Rushanthan <rushan26@live.com>
2017-03-14 19:12:44 +0530

    List of json items displaying through controller

---
+++ scripts/cribsController.js
@@ -0,0 +1,30 @@
+angular
+	.module('ngCribs')
+	.controller('cribsController', function($scope) {
+		$scope.cribs = [
+			{
+				"type" : "Condo",
+				"price" : "220000",
+				"address" : "213 Grove Street",
+				"description" : "Excellent place, real nice view!"
+			},
+			{
+				"type" : "House",
+				"price" : "452000",
+				"address" : "7854 Palm Street",
+				"description" : "Cool place, real nice view!"
+			},
+			{
+				"type" : "Duplex",
+				"price" : "1520000",
+				"address" : "888 York Street",
+				"description" : "Good neighbourhood!"
+			},
+			{
+				"type" : "Villa",
+				"price" : "2150000",
+				"address" : "44 Millenium Street",
+				"description" : "The Best!"
+			}
+		]
+	});
