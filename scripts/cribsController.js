84fb630fb61a90e23ba07c133585da6db6726308
Rushanthan <rushan26@live.com>
2017-03-17 14:43:50 +0530

    Display data from external json file

--- scripts/cribsController.js
+++ scripts/cribsController.js
@@ -1,5 +1,11 @@
 angular
 	.module('ngCribs')
 	.controller('cribsController', function($scope, cribsFactory) {
-		$scope.cribs = cribsFactory.getCribs();
+		$scope.cribs;
+
+		cribsFactory.getCribs().then(function(response) { 
+			$scope.cribs = response.data;
+		}, function(error) {
+			console.log(error);
+		});
 	});
--- scripts/cribsFactory.js
+++ scripts/cribsFactory.js
@@ -1,35 +1,9 @@
 angular
 	.module('ngCribs')
-	.factory('cribsFactory', function() {
-		var cribsData = [
-			{
-				"type" : "Condo",
-				"price" : "220000",
-				"address" : "213 Grove Street",
-				"description" : "Excellent place, real nice view!"
-			},
-			{
-				"type" : "House",
-				"price" : "452000",
-				"address" : "7854 Palm Street",
-				"description" : "Cool place, real nice view!"
-			},
-			{
-				"type" : "Duplex",
-				"price" : "1520000",
-				"address" : "888 York Street",
-				"description" : "Good neighbourhood!"
-			},
-			{
-				"type" : "Villa",
-				"price" : "2150000",
-				"address" : "44 Millenium Street",
-				"description" : "The Best!"
-			}
-		];
-		
+	.factory('cribsFactory', function($http) {
+				
 		function getCribs() {
-			return cribsData;
+			return $http.get('data/data.json');
 		}
 		
 		return {
